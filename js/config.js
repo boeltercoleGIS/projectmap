2438ccc7fe9fc75ad74835a6d6a80d7b30824593
boeltercoleGIS <boeltercole20@gmail.com>
2026-01-05 15:18:59 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -26,8 +26,8 @@ export const BASEMAPS = {
     label: 'Vector'
   },
   satellite: {
-    url: 'https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.jpg',
-    options: { maxZoom: 20, attribution: '&copy; CNES, Distribution Airbus DS, © Airbus DS, © PlanetObserver (Contains Copernicus Data) | &copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' },
+    url: 'https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryTopo/MapServer/tile/{z}/{y}/{x}',
+    options: { maxZoom: 20, attribution: 'Tiles courtesy of the <a href="https://usgs.gov/">U.S. Geological Survey</a>' },
     label: 'Satellite'
   }
 };
@@ -53,3 +53,4 @@ export const BOUNDARY = {
     fillOpacity: 0
   }
 };
+
